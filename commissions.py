f68f2b522d2d77e8da6cbd758aff28ae748a4dcc
Alexis Masson <165056049+vj013il@users.noreply.github.com>
2025-03-21 12:03:17 -0400

    Delete commissions.py

--- commissions.py
+++
@@ -1,4 +0,0 @@
-async def get_priority_fee(self):
-    # Adaptive commission through Jito
-    response = await self.client.get_recent_prioritization_fees()
-    return response[0].prioritization_fee * 1.1  # +10%
