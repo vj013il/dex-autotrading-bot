b7e5d0d29b547dda5041aa9b930e3098a54dde13
Lin <165056049+vj013il@users.noreply.github.com>
2025-04-29 02:25:25 -0400

    Delete notebooks directory

--- notebooks/docs/social.py
+++
@@ -1,5 +0,0 @@
-def generate_trade_alert(signal):
-    """Auto-post to Twitter/Discord"""
-    message = f"🚨 ALERT: {signal.coin} {signal.action} @ ${signal.price}"
-    post_to_twitter(message)
-    post_to_discord(message, channel='trading-alerts')
