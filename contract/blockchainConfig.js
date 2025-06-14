248d6eda1a6c9c2bf6f522b5e5922ba9a4db2bc4
Lin <165056049+vj013il@users.noreply.github.com>
2025-06-13 23:47:26 -0400

    Create blockchainConfig.js

---
+++ contract/blockchainConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ethereum: { rpcUrl: 'https://mainnet.infura.io/v3/YOUR_INFURA_KEY', chainId: 1 },
+  bsc: { rpcUrl: 'https://bsc-dataseed.binance.org/', chainId: 56 },
+  polygon: { rpcUrl: 'https://polygon-rpc.com/', chainId: 137 },
+  solana: { rpcUrl: 'https://api.mainnet-beta.solana.com', chainId: 'solana' }
+};
