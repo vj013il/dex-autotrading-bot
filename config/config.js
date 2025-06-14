34faa536559797f189304d45f97ce26060af5b22
Lin <165056049+vj013il@users.noreply.github.com>
2025-06-13 23:46:49 -0400

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 3000,
+  infuraUrl: process.env.INFURA_URL,
+  telegramToken: process.env.TELEGRAM_TOKEN,
+  telegramChatId: process.env.TELEGRAM_CHAT_ID,
+  masterPassword: process.env.MASTER_PASSWORD
+};
