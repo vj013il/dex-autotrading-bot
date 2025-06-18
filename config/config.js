caf8d3d64d39d401ca0491d931c9f5690940a19e
Lin <165056049+vj013il@users.noreply.github.com>
2025-06-18 06:05:14 -0400

    Delete config directory

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 3000,
-  infuraUrl: process.env.INFURA_URL,
-  telegramToken: process.env.TELEGRAM_TOKEN,
-  telegramChatId: process.env.TELEGRAM_CHAT_ID,
-  masterPassword: process.env.MASTER_PASSWORD
-};
